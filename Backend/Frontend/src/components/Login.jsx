7098eb4d0e8803fd8dac87ef889ca5ef825417d7
vishalpathak28 <165422642+vishalpathak28@users.noreply.github.com>
2025-09-03 23:22:19 +0530

    Update Login.jsx

--- Backend/Frontend/src/components/Login.jsx
+++ Backend/Frontend/src/components/Login.jsx
@@ -37,61 +37,61 @@ function Login() {
   };
 
   return (
-    <div>
-      <dialog id="my_modal_3" className="modal">
-        <div className="modal-box">
-          <form onSubmit={handleSubmit(onSubmit)} method="dialog">
-            <Link
-              to="/"
-              className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
-              onClick={() => document.getElementById("my_modal_3").close()}
-            >
-              ✕
-            </Link>
+    <dialog id="my_modal_3" className="modal">
+      <div className="modal-box">
+        <form onSubmit={handleSubmit(onSubmit)} method="dialog">
+          <Link
+            to="/"
+            className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+            onClick={() => document.getElementById("my_modal_3").close()}
+          >
+            ✕
+          </Link>
 
-            <h3 className="font-bold text-lg">Login</h3>
+          <h3 className="font-bold text-lg">Login</h3>
 
-            <div className="mt-4 space-y-2">
-              <span>Email</span>
-              <input
-                type="email"
-                placeholder="Enter your email"
-                className="w-80 px-3 py-1 border rounded-md outline-none"
-                {...register("email", { required: true })}
-              />
-              {errors.email && (
-                <span className="text-sm text-red-500">This field is required</span>
-              )}
-            </div>
+          {/* Email */}
+          <div className="mt-4 space-y-2">
+            <span>Email</span>
+            <input
+              type="email"
+              placeholder="Enter your email"
+              className="w-80 px-3 py-1 border rounded-md outline-none"
+              {...register("email", { required: true })}
+            />
+            {errors.email && (
+              <span className="text-sm text-red-500">This field is required</span>
+            )}
+          </div>
 
-            <div className="mt-4 space-y-2">
-              <span>Password</span>
-              <input
-                type="password"
-                placeholder="Enter your password"
-                className="w-80 px-3 py-1 border rounded-md outline-none"
-                {...register("password", { required: true })}
-              />
-              {errors.password && (
-                <span className="text-sm text-red-500">This field is required</span>
-              )}
-            </div>
+          {/* Password */}
+          <div className="mt-4 space-y-2">
+            <span>Password</span>
+            <input
+              type="password"
+              placeholder="Enter your password"
+              className="w-80 px-3 py-1 border rounded-md outline-none"
+              {...register("password", { required: true })}
+            />
+            {errors.password && (
+              <span className="text-sm text-red-500">This field is required</span>
+            )}
+          </div>
 
-            <div className="flex justify-around mt-6">
-              <button className="bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200">
-                Login
-              </button>
-              <p>
-                Not registered?{" "}
-                <Link to="/signup" className="underline text-blue-500 cursor-pointer">
-                  Signup
-                </Link>
-              </p>
-            </div>
-          </form>
-        </div>
-      </dialog>
-    </div>
+          <div className="flex justify-around mt-6">
+            <button className="bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200">
+              Login
+            </button>
+            <p>
+              Not registered?{" "}
+              <Link to="/signup" className="underline text-blue-500 cursor-pointer">
+                Signup
+              </Link>
+            </p>
+          </div>
+        </form>
+      </div>
+    </dialog>
   );
 }
 
