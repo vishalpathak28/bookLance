8caec316049cc87d76e1104125e6a5b087dfc3cf
vishalpathak28 <165422642+vishalpathak28@users.noreply.github.com>
2025-09-03 17:19:12 +0530

    Update main.jsx

--- Backend/Frontend/src/main.jsx
+++ Backend/Frontend/src/main.jsx
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import AuthProvider from "./context/AuthProvider.jsx";
+import AuthProvider from "./courses/context/AuthProvider.jsx";
+// import AuthProvider from "./context/AuthProvider.jsx";
 
 createRoot(document.getElementById('root')).render(
      <BrowserRouter>
