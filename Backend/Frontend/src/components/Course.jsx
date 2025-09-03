dac1d17381b083c388033df7457c23142ad8a66e
vishalpathak28 <165422642+vishalpathak28@users.noreply.github.com>
2025-09-03 21:04:43 +0530

    Update Course.jsx

--- Backend/Frontend/src/components/Course.jsx
+++ Backend/Frontend/src/components/Course.jsx
@@ -1,16 +1,15 @@
 import React, { useEffect, useState } from "react";
 import Cards from "./Cards";
-import axios from "axios";
-// import list from "../../public/list.json"
-// import axios from "axios";
+import API from "../api"; 
 import { Link } from "react-router-dom";
 
 function Course() {
   const [book, setBook] = useState([]);
+
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get("/book");
+        const res = await API.get("/book");
         console.log(res.data);
         setBook(res.data);
       } catch (error) {
@@ -19,32 +18,30 @@ function Course() {
     };
     getBook();
   }, []);
+
   return ( 
-    <>
-      <div className="pt-20 max-w-screen-2xl container mx-auto md:px-20 px-4 bg-white dark:bg-slate-900">
+    <div className="pt-20 max-w-screen-2xl container mx-auto md:px-20 px-4 bg-white dark:bg-slate-900">
+      <div className="mt-10 items-center justify-center text-center">
+        <h1 className="text-2xl md:text-4xl">
+          You've just unlocked awesome — <span className="text-pink-500">welcome!</span>
+        </h1>
+        <p className="mt-12">
+          Dive into a world of books where learning never stops. Whether you're preparing for exams, brushing up on skills, or exploring new topics, Bookstore is your trusted digital companion. Discover, read, and grow with us — one page at a time.
+        </p>
 
-        <div className="mt-10 items-center justify-center text-center">
-          <h1 className="text-2xl  md:text-4xl">
-            You've just unlocked awesome — {" "}
-            <span className="text-pink-500"> welcome!)</span>
-          </h1>
-          <p className="mt-12">
-            Dive into a world of books where learning never stops. Whether you're preparing for exams, brushing up on skills, or exploring new topics, Bookstore is your trusted digital companion. Discover, read, and grow with us — one page at a time.
-          </p>
+        <Link to="/">
+          <button className="mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300">
+            Back
+          </button>
+        </Link>
 
-           <Link to="/">
-            <button className="mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300">
-              Back
-            </button>
-          </Link>
         <div className="mt-12 grid grid-cols-1 md:grid-cols-4">
           {book.map((item) => (
             <Cards key={item.id} item={item} />
           ))}
         </div>
-        </div>
       </div>
-    </>
+    </div>
   );
 }
 
