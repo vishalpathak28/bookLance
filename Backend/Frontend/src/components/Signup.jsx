ff5fc3e47a8df86a741e54dfddd778d0c0382fed
Vishal Pathak <your-email@example.com>
2025-09-03 14:59:31 +0530

    Initial commit

--- Backend/Frontend/src/components/Signup.jsx
+++ Backend/Frontend/src/components/Signup.jsx
@@ -22,7 +22,7 @@ function Signup() {
       password: data.password,
     };
 
-    await axios.post("http://localhost:4001/user/signup", userInfo).then((res) => {
+    await axios.post("/user/signup", userInfo).then((res) => {
       console.log(res.data);
       if (res.data) {
         toast.success("Signup Successfully");
--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import bookRoute from "./route/book.route.js";
 import userRoute from "./route/user.route.js";
@@ -31,6 +32,21 @@ try {
 app.use("/book", bookRoute);
 app.use("/user", userRoute);
 
+//Deployment
+
+if (process.env.NODE_ENV === "production") {
+    const dirPath = path.resolve();
+    app.use(express.static(path.join(dirPath, "Frontend", "dist")));
+   app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(dirPath, "Frontend", "dist", "index.html"));
+});
+
+}
+
+ 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
+
+
+
