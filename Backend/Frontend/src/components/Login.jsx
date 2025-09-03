3286de6c15306c6ba68f74cd184149ef96d17f64
vishalpathak28 <165422642+vishalpathak28@users.noreply.github.com>
2025-09-03 23:34:45 +0530

    Update Login.jsx

--- Backend/Frontend/src/components/Login.jsx
+++ Backend/Frontend/src/components/Login.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
-import API from "../api"; 
+import axios from "axios";
 import toast from "react-hot-toast";
 
 function Login() {
@@ -16,7 +16,8 @@ function Login() {
       email: data.email,
       password: data.password,
     };
-    await API.post("/user/login", userInfo)
+    await axios
+      .post("https://booklance-1.onrender.com/user/login", userInfo)
       .then((res) => {
         console.log(res.data);
         if (res.data) {
@@ -37,61 +38,75 @@ function Login() {
   };
 
   return (
-    <dialog id="my_modal_3" className="modal">
-      <div className="modal-box">
-        <form onSubmit={handleSubmit(onSubmit)} method="dialog">
-          <Link
-            to="/"
-            className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
-            onClick={() => document.getElementById("my_modal_3").close()}
-          >
-            ✕
-          </Link>
+    <div>
+      <dialog id="my_modal_3" className="modal">
+        <div className="modal-box">
+          <form onSubmit={handleSubmit(onSubmit)} method="dialog">
+            <Link
+              to="/"
+              className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+              onClick={() => document.getElementById("my_modal_3").close()}
+            >
+              ✕
+            </Link>
 
-          <h3 className="font-bold text-lg">Login</h3>
+            <h3 className="font-bold text-lg">Login</h3>
 
-          {/* Email */}
-          <div className="mt-4 space-y-2">
-            <span>Email</span>
-            <input
-              type="email"
-              placeholder="Enter your email"
-              className="w-80 px-3 py-1 border rounded-md outline-none"
-              {...register("email", { required: true })}
-            />
-            {errors.email && (
-              <span className="text-sm text-red-500">This field is required</span>
-            )}
-          </div>
+            {/* Email */}
+            <div className="mt-4 space-y-2">
+              <span>Email</span>
+              <br />
+              <input
+                type="email"
+                placeholder="Enter your email"
+                className="w-80 px-3 py-1 border rounded-md outline-none"
+                {...register("email", { required: true })}
+              />
+              <br />
+              {errors.email && (
+                <span className="text-sm text-red-500">
+                  This field is required
+                </span>
+              )}
+            </div>
 
-          {/* Password */}
-          <div className="mt-4 space-y-2">
-            <span>Password</span>
-            <input
-              type="password"
-              placeholder="Enter your password"
-              className="w-80 px-3 py-1 border rounded-md outline-none"
-              {...register("password", { required: true })}
-            />
-            {errors.password && (
-              <span className="text-sm text-red-500">This field is required</span>
-            )}
-          </div>
+            {/* Password */}
+            <div className="mt-4 space-y-2">
+              <span>Password</span>
+              <br />
+              <input
+                type="password"
+                placeholder="Enter your password"
+                className="w-80 px-3 py-1 border rounded-md outline-none"
+                {...register("password", { required: true })}
+              />
+              <br />
+              {errors.password && (
+                <span className="text-sm text-red-500">
+                  This field is required
+                </span>
+              )}
+            </div>
 
-          <div className="flex justify-around mt-6">
-            <button className="bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200">
-              Login
-            </button>
-            <p>
-              Not registered?{" "}
-              <Link to="/signup" className="underline text-blue-500 cursor-pointer">
-                Signup
-              </Link>
-            </p>
-          </div>
-        </form>
-      </div>
-    </dialog>
+            {/* Button */}
+            <div className="flex justify-around mt-6">
+              <button className="bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200">
+                Login
+              </button>
+              <p>
+                Not registered?{" "}
+                <Link
+                  to="/signup"
+                  className="underline text-blue-500 cursor-pointer"
+                >
+                  Signup
+                </Link>
+              </p>
+            </div>
+          </form>
+        </div>
+      </dialog>
+    </div>
   );
 }
 
